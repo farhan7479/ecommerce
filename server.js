b839628e7a39e1741e516430e5e4a4ae15520328
farhan7479 <farhan078609@gmail.com>
2023-12-28 21:43:27 +0530

    es6-filename

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.use(morgan("dev"));
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+})
 
 
 
