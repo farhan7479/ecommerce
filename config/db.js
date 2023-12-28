cf2f6bdb3c2f78575b267fd843ff27e8d1410f54
farhan7479 <farhan078609@gmail.com>
2023-12-28 21:26:57 +0530

    es6-filename

--- config/db.js
+++ config/db.js
@@ -1,13 +1,24 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.error(`Error in MongoDB connection: ${error.message}`.bgRed.white);
+    process.exit(1); // Exit the application in case of connection failure
   }
 };
 
